ee015dec3b780f30c388a2e251aafcbbb2709e9b
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-10-20 19:32:02 +0800

    ETL: Update ETL

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,19 +1,17 @@
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 import requests
 
 def get_awards():
-    url = "https://web-production-b8145.up.railway.app/awards"
-    response = requests.get(url)
+    response = requests.get("https://web-production-b8145.up.railway.app/awards")
 
     if response.status_code == 200:
         return response.json()
     print(f"Failed to retrieve awards data: {response.status_code}")
 
-    return
+    return response
 
 def extract():
     # TODO: Implement film-awards-api extraction
+    awards = get_awards()
     
-    return
+    return awards
--- ETL/load.py
+++ ETL/load.py
@@ -1,2 +1,22 @@
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
 def load():
+    # TODO: Implement Full TMDB Movies Dataset 2024 (1M Movies) loading
+    # TODO: Implement Movies Metrics, Features and Statistics loading
+    
+    df1 = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        "asaniczka/tmdb-movies-dataset-2023-930k-movies",
+        "TMDB_movie_dataset_v11.csv",
+    )
+
+    df2 = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        "michaelmatta0/movies-ultimate-metrics-features-and-metadata",
+        "Top Movies (Cleaned Data).csv",
+    )
+
+    # TODO: Implement film-awards-api loading
+
     return
